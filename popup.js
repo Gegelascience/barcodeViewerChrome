c8f9d2d3730ce97661d5369df400be517bc321c1
VERCHERE Rémi <remi.verchere2@gmail.com>
2022-05-24 20:25:29 +0200

    ajout test ean 13

--- popup.js
+++ popup.js
@@ -4,11 +4,60 @@ var btn = document.getElementById("showEan")
 btn.addEventListener("click", () => {
   
   var inputValue = document.getElementById("ean").value
-  if(inputValue.length == 13) {
+  if(inputValue.length == 13 && isCorrectEan(inputValue)) {
     chrome.tabs.create({
       url: 'renderEan.html?ean=' + inputValue
     });
   } else {
-    alert(inputValue + "isn't an ean13")
+    alert(inputValue + " isn't an ean13")
   }
 })
+
+
+function isCorrectEan(inputValue) {
+  if (! inputValue){
+    return false
+  }
+        
+  var testLen = inputValue.length
+
+  if (testLen != 13){
+    return false
+  }
+
+  if (! (inputValue.match(/^[0-9]+$/) != null && parseInt(inputValue) >0)){
+    return false
+  }
+
+    var eanDigitLess = inputValue.slice(0,testLen-1)
+    var possibleDigitCheck = inputValue[testLen-1]
+    if (possibleDigitCheck != calculateDigitCheck(eanDigitLess)){
+        return false
+    }
+            
+    return true
+  
+}
+
+
+function calculateDigitCheck(eanDigitCheckLess) {
+
+  var lenstrCalcul = eanDigitCheckLess.length
+  var factor = 3
+  var somme = 0
+
+  if (! (eanDigitCheckLess.match(/^[0-9]+$/) != null && parseInt(eanDigitCheckLess) >0)){
+    return "KO"
+  }
+
+  for (let index = lenstrCalcul -1; index >-1 ; index--) {
+    somme += parseInt(eanDigitCheckLess[index]) * factor
+    factor = 4 - factor
+  }   
+      
+  digitCheck = ((10 - (somme % 10))%10).toString()
+
+  return digitCheck
+}
+   
+
