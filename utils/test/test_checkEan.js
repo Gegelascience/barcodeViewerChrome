cc1f2752f61d8f74029e6c6a53bd194b8170ab12
VERCHERE Rémi <remi.verchere2@gmail.com>
2023-07-23 14:57:02 +0200

    test coverage 100%

--- utils/test/test_checkEan.js
+++ utils/test/test_checkEan.js
@@ -6,11 +6,15 @@ describe('test check ean', function () {
   
     it('test isCorrectEan', function () {
       expect(isCorrectEan("12345670")).toBe(true);
+      expect(isCorrectEan("12345678")).toBe(false);
+      expect(isCorrectEan("123456780")).toBe(false);
+      expect(isCorrectEan("12H45670")).toBe(false);
+      expect(isCorrectEan()).toBe(false);
     });
 
     it('test calculateDigitCheck', function () {
-        expect(calculateDigitCheck("1234567")).toBe("0");
-      });
-  
+      expect(calculateDigitCheck("1234567")).toBe("0");
+      expect(calculateDigitCheck("123456K7")).toBe("KO");
+    });
 
   });
